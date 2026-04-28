ef3e673e511ea75026690f69c62ddfc15361fa02
aitorino2009 <aportalesc30@alumnes.santjosepobrer.es>
2026-04-28 17:50:30 +0200

    Implementación del logo

--- Componentes/Header/Header.js
+++ Componentes/Header/Header.js
@@ -1,6 +1,7 @@
 export function renderHeader() {
     return `
         <header class="header-component">
+            <img src="./Componentes/Persona 3 (Aitor)/img/logo.png" alt="Logo Club Deportivo Sant Josep Obrer" class="header-logo-img">
             <div class="header-content">
                 <div class="header-logo">
                     <span class="logo-icon">🏊</span>
